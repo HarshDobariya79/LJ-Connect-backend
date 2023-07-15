114712df1a2550c75b054d20c194bccb1fa94d5a
Harsh Dobariya <harshdobariya79@gmail.com>
2023-07-15 18:57:37 +0530

    refactor signal function

--- backend/core/data/signals.py
+++ backend/core/data/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import pre_save, post_save, pre_delete, post_delete, m2m_changed
 from django.dispatch import receiver
-from .models import Department
+from .models import Department, Batch
 from django.core.serializers import serialize
 import json
 from .utils import delete_empty_keys
@@ -10,62 +10,66 @@ from .utils import delete_empty_keys
 @receiver(post_save, sender=Department)
 @receiver(m2m_changed, sender=Department.batch.through)
 def handle_department_modifications(sender, instance, action=None, **kwargs):
-    
-    if action == 'post_add' or action == 'post_remove' or action == None:
 
-        hod = instance.hod
+    try:
+        if action == 'post_add' or action == 'post_remove' or action == None:
 
-        permissions_json = {
-            instance.year: {
-                instance.semester: {
-                    instance.name: {
+            serialized_data = json.loads(serialize('json', instance.batch.all()))
+            hod = instance.hod
+            updated_batch_list = list(map(lambda x: x["fields"]["name"], serialized_data))
+            
+            if action == 'post_remove':
+                department = hod.permissions[instance.year][instance.semester][instance.name]
+                for batch, _ in department.items():
+                    if(batch not in updated_batch_list):
+                        del department[batch]
+                        
+            else:
 
+                permissions_json = {
+                    instance.year: {
+                        instance.semester: {
+                            instance.name: {
+
+                            }
+                        }
                     }
                 }
-            }
-        }
 
-        serialized_data = json.loads(serialize('json', instance.batch.all()))
-        updated_batch_list = list(map(lambda x: x["fields"]["name"], serialized_data))
-        
-        for batch in updated_batch_list:
-            permissions_json[instance.year][instance.semester][instance.name][batch] = {
-                "attendance": {
-                    "create": True,
-                    "read": True,
-                    "update": True,
-                    "delete": True
-                },
-                "test_result": {
-                    "create": True,
-                    "read": True,
-                    "update": True,
-                    "delete": True
-                },
-                "project": {
-                    "create": True,
-                    "read": True,
-                    "update": True,
-                    "delete": True
-                },
-                "mooc": {
-                    "create": True,
-                    "read": True,
-                    "update": True,
-                    "delete": True
-                }
-            }
+                for batch in updated_batch_list:
+                    permissions_json[instance.year][instance.semester][instance.name][batch] = {
+                        "attendance": {
+                            "create": True,
+                            "read": True,
+                            "update": True,
+                            "delete": True
+                        },
+                        "test_result": {
+                            "create": True,
+                            "read": True,
+                            "update": True,
+                            "delete": True
+                        },
+                        "project": {
+                            "create": True,
+                            "read": True,
+                            "update": True,
+                            "delete": True
+                        },
+                        "mooc": {
+                            "create": True,
+                            "read": True,
+                            "update": True,
+                            "delete": True
+                        }
+                    }
 
-        hod.permissions.update(permissions_json)
-        department = hod.permissions[instance.year][instance.semester][instance.name]
+                hod.permissions.update(permissions_json)
+            delete_empty_keys(hod.permissions)
+            hod.save()
 
-        if action == 'post_remove':
-            for batch, _ in department.items():
-                if(batch not in updated_batch_list):
-                    del department[batch]
-        delete_empty_keys(hod.permissions)
-
-        hod.save()
+    except Exception as e:
+        print(e)
 
 @receiver(pre_save, sender=Department)
 @receiver(pre_delete, sender=Department)
