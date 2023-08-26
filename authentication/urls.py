b58278f3e145fc02100175538eca8ce35e191946
Vishwa Raval <vishwaraval247@gmail.com>
2023-08-26 11:27:16 +0530

    API endpoint for retrieving user profile details and role

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import GoogleLoginView
+from .views import GoogleLoginView, ProfileView
 
 urlpatterns = [
+    # path('accounts/', include('allauth.urls')),
+    # path('accounts/social/', include('allauth.socialaccount.urls')),
     path("v1/google/login/", GoogleLoginView.as_view(), name="google_login"),
     path("v1/refresh-token/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("v1/profile/", ProfileView.as_view(), name="profile"),
 ]
