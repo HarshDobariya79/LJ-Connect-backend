d5d416053aecb6a70facecc897873fd949774a70
Vishwa Raval <vishwaraval247@gmail.com>
2023-08-26 11:35:18 +0530

    remove unwanted comments

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,8 +4,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import GoogleLoginView, ProfileView
 
 urlpatterns = [
-    # path('accounts/', include('allauth.urls')),
-    # path('accounts/social/', include('allauth.socialaccount.urls')),
     path("v1/google/login/", GoogleLoginView.as_view(), name="google_login"),
     path("v1/refresh-token/", TokenRefreshView.as_view(), name="token_refresh"),
     path("v1/profile/", ProfileView.as_view(), name="profile"),
