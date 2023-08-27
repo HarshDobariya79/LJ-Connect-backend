30c3ea13c5057c621bba0869e691fc1f44300d05
Harsh Dobariya <harshdobariya79@gmail.com>
2023-08-27 21:19:07 +0530

    Permission class for admin status check

--- authentication/authentication_classes.py
+++ authentication/authentication_classes.py
@@ -6,71 +6,6 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from data.models import StaffDetail, StudentDetail, StudentSemesterRecord
 
 
-class IsActiveStaff(BaseAuthentication):
-    def authenticate(self, request):
-        auth_header = request.headers.get("Authorization")
-        if auth_header is None:
-            raise AuthenticationFailed("Authorization header missing.")
-
-        auth_token = auth_header.split(" ")
-        if len(auth_token) != 2 or auth_token[0] != "Bearer":
-            raise AuthenticationFailed("Invalid authorization header.")
-
-        token = auth_token[1]
-
-        try:
-            jwt_authentication = JWTAuthentication()
-            validated_token = jwt_authentication.get_validated_token(token)
-
-            user = jwt_authentication.get_user(validated_token)
-
-            staff_data = StaffDetail.objects.filter(email=user.email, active=True)
-
-            if not staff_data:
-                raise AuthenticationFailed("Access not allowed.")
-
-            return (user, None)
-
-        except AuthenticationFailed as e:
-            raise e
-        except Exception:
-            raise AuthenticationFailed("Something went wrong")
-
-
-class IsActiveStudent(BaseAuthentication):
-    def authenticate(self, request):
-        auth_header = request.headers.get("Authorization")
-        if auth_header is None:
-            raise AuthenticationFailed("Authorization header missing.")
-
-        auth_token = auth_header.split(" ")
-        if len(auth_token) != 2 or auth_token[0] != "Bearer":
-            raise AuthenticationFailed("Invalid authorization header.")
-
-        token = auth_token[1]
-
-        try:
-            jwt_authentication = JWTAuthentication()
-            validated_token = jwt_authentication.get_validated_token(token)
-
-            user = jwt_authentication.get_user(validated_token)
-
-            student_obj = StudentSemesterRecord.objects.filter(
-                student__email=user.email, student__graduated=False
-            )
-            print(student_obj)
-
-            if not student_obj:
-                raise AuthenticationFailed("Access not allowed.")
-
-            return (user, None)
-
-        except AuthenticationFailed as e:
-            raise e
-        except Exception:
-            raise AuthenticationFailed("Something went wrong.")
-
-
 class IsAuthenticatedWithToken(BaseAuthentication):
     def authenticate(self, request):
         auth_header = request.headers.get("Authorization")
@@ -87,7 +22,6 @@ class IsAuthenticatedWithToken(BaseAuthentication):
 
         token = auth_token[1]
 
-        # Here, you might want to perform additional checks on the token, like its validity or expiration.
         try:
             jwt_authentication = JWTAuthentication()
             validated_token = jwt_authentication.get_validated_token(token)
