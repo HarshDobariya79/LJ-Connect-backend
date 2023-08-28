149ebd313f785d7fc321532115879425edf2ecb5
Vishwa Raval <vishwaraval247@gmail.com>
2023-08-28 10:08:22 +0530

    fix pre-commit hooks

--- data/signals.py
+++ data/signals.py
@@ -2,8 +2,13 @@ import json
 from copy import deepcopy
 
 from django.core.serializers import serialize
-from django.db.models.signals import (m2m_changed, post_delete, post_save,
-                                      pre_delete, pre_save)
+from django.db.models.signals import (
+    m2m_changed,
+    post_delete,
+    post_save,
+    pre_delete,
+    pre_save,
+)
 from django.dispatch import receiver
 
 from .models import Batch, Department, FacultyAllocation
