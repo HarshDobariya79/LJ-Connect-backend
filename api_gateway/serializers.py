aa900c14e70004472e815a1e73af4ebfb41fa068
Harsh Dobariya <harshdobariya79@gmail.com>
2023-08-31 15:29:22 +0530

    Create Read update APIs for Branch modal

--- api_gateway/serializers.py
+++ api_gateway/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from data.models import StaffDetail
+from data.models import Branch, StaffDetail
 
 
 class StaffDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = StaffDetail
+        fields = "__all__"
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
         fields = "__all__"
--- api_gateway/urls.py
+++ api_gateway/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import StaffDetailAPI
+from .views import BranchAPI, StaffDetailAPI
 
 urlpatterns = [
     path("v1/staff/", StaffDetailAPI.as_view(), name="staff-list"),
+    path("v1/branch/", BranchAPI.as_view(), name="branch-list"),
 ]
